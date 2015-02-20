4427f08932cbfea0ecba39a7b6d08b05467286e1
Edgar Z. Alvarenga <e@vaz.io>
2015-02-20 11:46:21 -0200

    Printing the number fractional series

--- collatz.hs
+++ collatz.hs
@@ -20,3 +20,11 @@ find_x eks@(a:e_k) = 2**(fromIntegral a)/3**(fromIntegral $ k - 1) * (1 - sum_ek
 	where 
 		sum_ek = sum $ [ 3**(fromIntegral i)/2**(fromIntegral e) | (i, e) <- zip [k-2,k-3..0] e_k ]
 	      	k = length eks
+
+to_latex n = (show n) ++ " = \\frac{2^{" ++ e_k ++ "}}{3^{" ++  show k ++  "}}\\Big[ 1 - " ++ the_sum ++ "\\Big]"
+	where 
+		ns = calcs n	
+		(e_k:e) = map (show.snd) ns 
+		k = length e	
+		the_sum_l = map (\(a,b) -> "\\frac{3^{" ++ show a ++ "}}{2^{" ++ show b ++ "}}") $ tail ns
+		the_sum = foldl (\a b -> a ++ " - " ++ b) (head the_sum_l) $ tail the_sum_l
